e9f4661c146c550f61de0480fc34c0e4973ca845
peter huyser <huyserp@gmail.com>
2021-01-14 20:18:10 +0100

    completed through index view

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,33 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
